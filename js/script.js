b9fab9076a421097dbc5b53fccddbebe40b1d9c8
Anthony <ajsur1017@gmail.com>
2021-08-25 20:51:47 -0400

    update

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const $weather = $('#weather');
 const $temp = $('#temp');
 const $feels_like = $('#feelsLike');
 
-const $input = $('input[type="text"]');
+const $input = $('input[type="text"]'); 
+const $input2 = $('#city');
 
 function handleGetData(event){
     event.preventDefault();
@@ -31,8 +32,32 @@ function render(){
     $feels_like.text(weatherData.main.feels_like);
     $weather.text(weatherData.weather[0].description);
 }
+function handleGetData2(event){
+    event.preventDefault();
+    console.log("Form Submitted")
+    $.ajax({
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${$input2.val()}&appid=ae3a4765bf66840762fe0ddea8bb7dfb&units=imperial` 
+    }).then(
+        function(data){
+         console.log(data); 
+         weatherData = data;
+         render2(); 
+         $input2.val("") 
+        },
+        function(error){
+         console.log('bad request', error);
+        }
+      );
+}
 
+function render2(){
+    $weatherFor.text(weatherData.name);
+    $temp.text(weatherData.main.temp);
+    $feels_like.text(weatherData.main.feels_like);
+    $weather.text(weatherData.weather[0].description);
+}
 
-$('form').on('submit', handleGetData)
+$('#form1').on('submit', handleGetData)
+$('#form2').on('submit', handleGetData2)
 
 
